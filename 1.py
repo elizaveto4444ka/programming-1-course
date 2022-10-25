98ef29d80ed5da992c02b30b3fe2ff0fc0a12273
elizaveto4444ka <114440286+elizaveto4444ka@users.noreply.github.com>
2022-10-25 16:31:09 +0200

    Все задания добавлены к лекции 1

---
+++ 1.py
@@ -0,0 +1,27 @@
+name = input()
+print("Привет,", name)
+
+r = input()
+one = input()
+two = input()
+three = input()
+print(one+r+two+r+three)
+
+a = int(input())
+print("Объем = ", a**3)
+print('Площадь полной поверхности = ', 6*a**2)
+
+a1 = int(input())
+d = int(input())
+n = int(input())
+print(a1 + d*(n-1))
+
+n = int(input())
+k = int(input())
+print(k//n)
+print(k%n)
+
+m = int(input())
+hours = m//60
+minutes = m%60
+print(m , 'мин - это' , hours , 'час' , minutes , 'минут.')
